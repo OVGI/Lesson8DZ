aeeefd8467fd36dbdc08bd02d0da8ac9894f9d57
OVGI <girichevao@gmail.com>
2022-12-19 18:45:50 +0300

    add task_58

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18      */
+
+int[,] a = new int[3, 2] { { 3, 5 }, { 4, 3 }, { 2, 8 } };
+int[,] b = new int[2, 3] { { 4, 6, 3 }, { 5, 7, 2 } };
+int[,] c = new int[a.GetLength(0), b.GetLength(1)];
+for (int i = 0; i < c.GetLength(0); i++)
+{
+    for (int j = 0; j < c.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int r = 0; r < a.GetLength(1); r++)
+        {
+            sum += a[i, r] * b[r, j];
+        }
+        c[i, j] = sum;
+    }
+}
+
+for (int i = 0; i < c.GetLength(0); i++)
+{
+    for (int j = 0; j < c.GetLength(1); j++)
+        Console.Write($"{c[i, j]} ");
+    Console.WriteLine();
+}
